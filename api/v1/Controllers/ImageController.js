8d64b38a2d5d14ab7fb3d63a853404ebbbd3fd4f
Rehan Shakeel <rehanshakeel.akhtar@epsystems.pk>
2023-03-16 11:43:13 +0500

    removing img after uploading

--- api/v1/Controllers/ImageController.js
+++ api/v1/Controllers/ImageController.js
@@ -1,5 +1,8 @@
 const tryCatchAsync = require("../../../util/tryCatchAsync");
 const apiResponse = require("../../../util/apiResponse");
+const fs = require('fs')
+const { promisify } = require('util')
+const unlinkAsync = promisify(fs.unlink)
 const cloudinary = require("cloudinary").v2;
 cloudinary.config({
   cloud_name: "dyxfd7ura",
@@ -15,6 +18,7 @@ exports.imageUpload = tryCatchAsync(async (req, res, next) => {
     const orginalName = file.originalname;
     const resp = await cloudinary.uploader.upload(path, { public_id: filename });
     console.log("res", resp);
+    await unlinkAsync(path)
     response_data = { url: resp.secure_url };
   } catch (error) {
     console.log(error);
